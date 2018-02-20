0af9f90e5a56f153a67ad5aab1d70a30a028b05b
RAY HTOO - STUDENT <G00356245@gmit.ie>
2018-02-20 13:54:58 +0000

    Break_ex3

---
+++ Break_Ex3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Break_Ex3 {
+	public static void main(String []args){
+	Scanner input = new Scanner (System.in);
+	int counter= 0;
+
+	System.out.println("Guess a number from 1 to 10");
+	System.out.println("you have 10 attempts:");
+
+	while(input.hasNext()){
+		if((input.nextInt() ==8) || (counter == 10)){
+			System.out.println("you guess right (8), game over");
+			break;
+		}
+		counter++;
+		if(counter == 10){
+			System.out.println("10 attempts only");
+			break;
+		}
+	}
+}
+}
