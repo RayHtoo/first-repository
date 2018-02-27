4fb0f389620e551e21aff3c2613038bb8a0b6625
RayHtoo <36404670+RayHtoo@users.noreply.github.com>
2018-02-27 13:55:08 +0000

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,21 @@
+public class Methods{
+	public static void main(String []args){
+		Methods m = new Methods();
+		m.method1();
+		Index.main(null);
+	}
+
+	public void method1(){
+	System.out.println("Method 1 here and i am calling method 2");
+	method2();
+}
+
+	public void method2(){
+	System.out.println("Hi Method 1,from method 2,i am calling method 3");
+	method3();
+}
+	public void method3(){
+	System.out.println("Hi method 2, from method 3, tell method 1 I say hello.");
+
+	}
+}
